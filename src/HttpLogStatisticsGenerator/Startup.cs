c9190c5de56d427a9d9979736ff14f5b74d41185
SK <sk.mechatronics@outlook.com>
2021-08-14 22:34:49 +1000

    Cleanup startup. Add test project

--- src/HttpLogStatisticsGenerator/Startup.cs
+++ src/HttpLogStatisticsGenerator/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using HttpLogStatisticsGenerator.Input;
 using HttpLogStatisticsGenerator.Statistics;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Serilog;
 using System.Threading.Tasks;
@@ -8,38 +9,83 @@ namespace HttpLogStatisticsGenerator
 {
     public class Startup
     {
+        private readonly ServiceCollection services;
+        private readonly IConfigurationRoot configuration;
+        private ServiceProvider serviceProvider;
+
+        public Startup()
+        {
+            this.services = new ServiceCollection();
+            this.configuration = new ConfigurationBuilder()
+                                .AddJsonFile("appsettings.json")
+                                .Build();
+        }
+
         public static async Task Main()
         {
             var program = new Startup();
-            var services = program.Setup();
-            var runner = services.GetService<Runner>();
-
-            await runner.Run();
+            program.Setup();
+            await program.Run();            
         }
 
-        private ServiceProvider Setup()
+        private void Setup()
+        {
+            this.AddConfiguration()
+                .AddLogging()
+                .AddInputProcessors()
+                .AddGenerators()
+                .BuildServiceProvider();
+        }
+
+        private Startup AddConfiguration()
+        {
+            var statisticsConfiguration = this.configuration.GetSection(nameof(StatisticsGeneratorConfiguration));
+            this.services.Configure<StatisticsGeneratorConfiguration>(statisticsConfiguration);
+            return this;
+        }
+
+        private Startup AddLogging()
         {
             var loggingConfiguration = new LoggerConfiguration()
                                         .WriteTo.Console();
+            services
+               .AddLogging(logging => logging.AddSerilog(loggingConfiguration.CreateLogger()));
 
-            var config = new StatisticsGeneratorConfiguration
-            {
-                InputFileLogPath = @"D:\_codingtest\azenix\programming-task\programming-task-example-data.log"
-            };
+            return this;
+        }
 
-            var serviceProvider = new ServiceCollection()
-                .AddLogging(logging => logging.AddSerilog(loggingConfiguration.CreateLogger()))
-                .AddSingleton<StatisticsGeneratorConfiguration>(config)
+        private Startup AddInputProcessors()
+        {
+            this.services
                 .AddSingleton<IHttpLogReader, FileHttpLogReader>()
                 .AddSingleton<IHttpLogTokenizer, HttpLogTokenizer>()
-                .AddSingleton<IHttpLogParser, HttpInputParser>()
+                .AddSingleton<IHttpLogParser, HttpInputParser>();
+
+            return this;
+        }
+
+        private Startup AddGenerators()
+        {
+            this.services
                 .AddSingleton<IStatisticsGenerator, UniqueIpAddressStatistic>()
                 .AddSingleton<IStatisticsGenerator, TopIpAddressGenerator>()
                 .AddSingleton<IStatisticsGenerator, TopUrlStatisticGenerator>()
-                .AddSingleton<Runner>()
-                .BuildServiceProvider();
+                .AddSingleton<Runner>();
 
-            return serviceProvider;
+            return this;
         }
+
+        private Startup BuildServiceProvider()
+        {
+            this.serviceProvider = this.services.BuildServiceProvider();
+            return this;
+        }
+
+        private async Task Run()
+        {
+            var runner = this.serviceProvider.GetService<Runner>();
+            await runner.Run();
+        }
+
     }
 }
